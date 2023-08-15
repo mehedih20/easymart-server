ce42db645eed990abc9d3fae6b206f34cdc9b315
mehedih20 <mehedih20@gmail.com>
2023-08-15 16:51:35 +0530

    all functionality done

--- index.js
+++ index.js
@@ -37,9 +37,30 @@ async function run() {
       const result = await productCollection.find({}).toArray();
       res.json(result);
     });
+    // Get single products
+    app.get("/products/:id", async (req, res) => {
+      const id = ObjectId(req.params.id);
+      const query = { _id: id };
+      const result = await productCollection.findOne(query);
+      res.json(result);
+    });
+
     // Get all users
     app.get("/users", async (req, res) => {
       const result = await userCollection.find({}).toArray();
+      res.json(result);
+    });
+    // Get all users
+    app.get("/orders", async (req, res) => {
+      const result = await orderCollection.find({}).toArray();
+      res.json(result);
+    });
+
+    // Get single user
+    app.get("/users/:email", async (req, res) => {
+      const uesrEmail = req.params.email;
+      const query = { email: uesrEmail };
+      const result = await userCollection.findOne(query);
       res.json(result);
     });
 
@@ -49,6 +70,13 @@ async function run() {
     app.post("/products", async (req, res) => {
       const product = req.body;
       const result = await productCollection.insertOne(product);
+      res.json(result);
+    });
+
+    //Create an order
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
       res.json(result);
     });
 
@@ -66,6 +94,7 @@ async function run() {
 
     //------------ Updating ---------------
 
+    // Updating product info
     app.put("/product/:id", async (req, res) => {
       const id = ObjectId(req.params.id);
       const { category, name, imgUrl, price, oldPrice, rating, deal } =
@@ -90,6 +119,98 @@ async function run() {
       );
       res.json(result);
       console.log(result);
+    });
+
+    //Making admin
+    app.put("/user/makeAdmin/:id", async (req, res) => {
+      const id = ObjectId(req.params.id);
+      const filter = { _id: id };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    });
+
+    //Removing admin
+    app.put("/user/removeAdmin/:id", async (req, res) => {
+      const id = ObjectId(req.params.id);
+      const filter = { _id: id };
+      const options = { upsert: true };
+      const updateDoc = {
+        $unset: {
+          role: 1,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    });
+
+    //Updating cart
+    app.put("/user/cart/:email", async (req, res) => {
+      const userEmail = req.params.email;
+      const filter = { email: userEmail };
+      const newCartItem = req.body;
+      const options = { upsert: true };
+      const updateDoc = {
+        $push: {
+          cart: newCartItem,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    });
+
+    //Removing item from cart
+    app.put("/user/cart/removeItem/:email", async (req, res) => {
+      const userEmail = req.params.email;
+      const filter = { email: userEmail };
+      const pId = req.body.itemId;
+      const options = { upsert: true };
+      const updateDoc = {
+        $pull: {
+          cart: {
+            itemId: pId,
+          },
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    });
+
+    //Removing cart after placing order
+    app.put("/cart/orderConfirmed/:email", async (req, res) => {
+      const userEmail = req.params.email;
+      const filter = { email: userEmail };
+      const options = { upsert: true };
+      const updateDoc = {
+        $unset: {
+          cart: 1,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    });
+
+    //Updating order status
+    app.put("/orders/:id", async (req, res) => {
+      const orderId = ObjectId(req.params.id);
+      const filter = { _id: orderId };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: "shipped",
+        },
+      };
+      const result = await orderCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
     });
 
     //------------ Deleting ---------------
